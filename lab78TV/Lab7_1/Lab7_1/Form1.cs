a71b39b66579e28bb13fd071509923c7b3d83f5e
rosku <speedragons@gmail.com>
2019-11-20 00:23:43 +0200

    lab78TV

---
+++ lab78TV/Lab7_1/Lab7_1/Form1.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab7_1
+{
+    public partial class Form1 : Form
+    {
+        int i = 0;
+        int ans_index = 0;
+        int right = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Text = "";
+            textBox1.Visible = false;
+            button2.Visible = false;
+
+            label1.Text = questions[i];
+            radioButton1.Text = answers[ans_index];
+            radioButton2.Text = answers[ans_index + 1];
+            radioButton3.Text = answers[ans_index + 2];
+            radioButton4.Text = answers[ans_index + 3];
+
+        }
+
+        string[] questions = new string[]
+{   "What is 9 cubed?", "What is 6+3?",
+    "What type of animal is tuna sandwiches made from?",
+    "What is 18 backwards?",
+    "What color is the snow?",
+    "What is the capital of Bulgaria?"
+};
+        string[] answers = new string[] {
+   "9", "81", "729", "2",
+   "4", "2", "9", "1",
+   "zebra", "aardvark", "fish", "gnu",
+   "31", "81", "91", "88",
+   "Red", "Green", "White", "Violet",
+   "Varna", "Shumen", "Sofia", "Plovdiv"
+};
+        string[] quizAnswers = new string[] { "729", "9", "aardvark", "81", "White","Sofia" };
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch (i)
+            {
+                case 0:
+                    if (radioButton3.Checked == true)
+                        right++;
+                    break;
+                case 1:
+                    if (radioButton3.Checked == true)
+                        right++;
+                    break;
+                case 2:
+                    if (radioButton3.Checked == true)
+                        right++;
+                    break;
+                case 3:
+                    if (radioButton2.Checked == true)
+                        right++;
+                    break;
+                case 4:
+                    if (radioButton3.Checked == true)
+                        right++;
+                    break;
+
+            }
+            radioButton1.Checked = true;
+            if (i == 4)
+            {
+                i++;
+                ans_index += 4;
+                label1.Text = questions[i];
+                radioButton1.Text = answers[ans_index];
+                radioButton2.Text = answers[ans_index + 1];
+                radioButton3.Text = answers[ans_index + 2];
+                radioButton4.Text = answers[ans_index + 3];
+                button1.Visible = false; button2.Visible = true;
+            }
+            else { 
+                i++;
+                ans_index += 4;
+                label1.Text = questions[i];
+                radioButton1.Text = answers[ans_index];
+                radioButton2.Text = answers[ans_index + 1];
+                radioButton3.Text = answers[ans_index + 2];
+                radioButton4.Text = answers[ans_index + 3];
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (radioButton3.Checked == true)
+            {
+                right++;
+            }
+            MessageBox.Show("End of Test");
+            try
+            {
+                if (right == 6)
+                {
+                    pictureBox1.BackgroundImage = Image.FromFile("C:/Users/Admin/source/repos/Lab7_1/frog.jpg");
+                    pictureBox1.BackgroundImageLayout = ImageLayout.Stretch;
+                }
+            }catch { MessageBox.Show("Image file not found"); }
+
+            label2.Text = "Result:";
+            textBox1.Visible = true;
+            textBox1.Text = right.ToString()+" out of 6";
+        }
+    }
+}
---
+++ lab78TV/Lab8_1 2/Lab8_1/Form1.cs
@@ -0,0 +1,301 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab8_1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            textBox1.TextAlign = HorizontalAlignment.Right;
+            textBox1.Enabled = false;
+        }
+
+        string s="";
+        double sum=0;
+        double num=0;
+        int operation = 0;
+
+        /*buttons 1 - 10
+         * sa chislata posledovatelno
+         */
+        private void button1_Click(object sender, EventArgs e)
+        {
+            s += button1.Text;
+            textBox1.Text = s;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            s += button2.Text;
+            textBox1.Text = s;
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            s += button3.Text;
+            textBox1.Text = s;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            s += button4.Text;
+            textBox1.Text = s;
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            s += button5.Text;
+            textBox1.Text = s;
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            s += button6.Text;
+            textBox1.Text = s;
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            s += button7.Text;
+            textBox1.Text = s;
+        }
+        private void button8_Click(object sender, EventArgs e)
+        {
+            s += button8.Text;
+            textBox1.Text = s;
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            s += button9.Text;
+            textBox1.Text = s;
+        }
+
+        private void button10_Click(object sender, EventArgs e)
+        {
+            s += button10.Text;
+            textBox1.Text = s;
+        }
+
+        //plus button
+        private void button16_Click(object sender, EventArgs e)
+        {
+            button12.Enabled = true;
+            operation = 1;
+            s = "";
+            sum += Double.Parse(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        //subtract button
+        private void button15_Click(object sender, EventArgs e)
+        {
+            button12.Enabled = true;
+            operation = 2;
+            s = "";
+            sum -= Double.Parse(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        //multiply button
+        private void button14_Click(object sender, EventArgs e)
+        {
+            button12.Enabled = true;
+            sum = 1;
+            operation = 3;
+            s = "";
+            sum *= Double.Parse(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        //divide button
+        private void button13_Click(object sender, EventArgs e)
+        {
+            button12.Enabled = true;
+            sum = 1;
+            operation = 4;
+            s = "";
+            sum *= Double.Parse(textBox1.Text);
+            textBox1.Text = "";
+        }
+
+        //clear button
+        private void button18_Click(object sender, EventArgs e)
+        {
+            button12.Enabled = true;
+            s = "";
+            textBox1.Text = "";
+        }
+
+        //equals button
+        private void button17_Click(object sender, EventArgs e)
+        {
+            button12.Enabled = true;
+            switch (operation)
+            {
+                case 1:
+                    num = Double.Parse(textBox1.Text);
+                    sum += num;
+                    break;
+                case 2:
+                    num = Double.Parse(textBox1.Text);
+                    sum = -num - sum;
+                    break;
+                case 3:
+                    num = Double.Parse(textBox1.Text);
+                    sum = num * sum;
+                    break;
+                case 4:
+                    num = Double.Parse(textBox1.Text);
+                    sum = sum / num;
+                    break;
+            }
+            textBox1.Text = (sum).ToString();
+            s = sum.ToString();
+            sum = 0;
+        }
+
+        //decimal button
+        private void button12_Click(object sender, EventArgs e)
+        {
+            s += button12.Text;
+            textBox1.Text = s;
+            button12.Enabled = false;
+        }
+
+        //+/- button
+        private void button11_Click(object sender, EventArgs e)
+        {
+            s = (Double.Parse(s) * -1).ToString();
+            textBox1.Text = s;
+        }
+
+        //ToBinary button
+        private void button29_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                int value = Int32.Parse(s);
+                string binary = Convert.ToString(value, 2);
+                richTextBox1.Text = binary;
+            }
+            catch { richTextBox1.Text = "No number is entered"; }
+        }
+
+        //10^x button
+        private void button20_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                sum = 1;
+                for (int i = 0; i < Int32.Parse(s); i++)
+                {
+                    sum *= 10;
+                }
+                s = sum.ToString();
+                textBox1.Text = s;
+                sum = 0;
+            }
+            catch { textBox1.Text = "Select number first"; }
+        }
+
+        //sqrt button
+        private void button19_Click(object sender, EventArgs e)
+        {
+
+            sum = Math.Sqrt(Double.Parse(s));
+            s = sum.ToString();
+            textBox1.Text = s;
+            sum = 0;
+        }
+
+        //sin button
+        private void button24_Click(object sender, EventArgs e)
+        {
+            sum = Math.Sin(Double.Parse(s));
+            s = sum.ToString();
+            textBox1.Text = s;
+            sum = 0;
+        }
+
+        //cos button
+        private void button27_Click(object sender, EventArgs e)
+        {
+            sum = Math.Cos(Double.Parse(s));
+            s = sum.ToString();
+            textBox1.Text = s;
+            sum = 0;
+        }
+
+        //tg button
+        private void button26_Click(object sender, EventArgs e)
+        {
+            sum = Math.Tan(Double.Parse(s));
+            s = sum.ToString();
+            textBox1.Text = s;
+            sum = 0;
+        }
+
+        //x^2
+        private void button22_Click(object sender, EventArgs e)
+        {
+            sum = Double.Parse(s);
+            sum = sum * sum;
+            s = sum.ToString();
+            textBox1.Text = s;
+            sum = 0;
+        }
+
+        //x^3
+        private void button23_Click(object sender, EventArgs e)
+        {
+            sum = Double.Parse(s);
+            sum = sum * sum * sum;
+            s = sum.ToString();
+            textBox1.Text = s;
+            sum = 0;
+        }
+
+        //log10
+        private void button21_Click(object sender, EventArgs e)
+        {
+            sum = Math.Log10(Double.Parse(s));
+            s = sum.ToString();
+            textBox1.Text = s;
+            sum = 0;
+        }
+
+        //Pi
+        private void button25_Click(object sender, EventArgs e)
+        {
+            s = Math.PI.ToString();
+            textBox1.Text = s;
+        }
+
+        //n!
+        private void button28_Click(object sender, EventArgs e)
+        {
+            double num = Int32.Parse(s);
+            sum = 1;
+
+            while(num!=0)
+            {
+                sum *= num;
+                num--;
+            }
+
+            s = sum.ToString();
+            textBox1.Text = s;
+            sum = 0;
+        }
+    }
+}
